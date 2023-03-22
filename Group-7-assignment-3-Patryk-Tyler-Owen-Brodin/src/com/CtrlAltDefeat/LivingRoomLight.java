e2fa9048cc189c43ec921049136a20d2bacf0d43
Owen Garland <Ogarland9238@conestogac.on.ca>
2023-03-21 21:37:54 -0400

    living room light completed

---
+++ Group-7-assignment-3-Patryk-Tyler-Owen-Brodin/src/com/CtrlAltDefeat/LivingRoomLight.java
@@ -0,0 +1,14 @@
+package com.CtrlAltDefeat;
+
+public class LivingRoomLight extends Light{
+@Override
+public void on() {
+	
+	System.out.println("Turn on the Living room Light");
+}
+@Override
+public void off() {
+	
+	System.out.println("Turn off the Living room Light");
+}
+}
